4cc00a587675ad25878508a95107ed0f093b344d
PascalMichael <pascalgwami@gmail.com>
2025-09-27 04:15:14 +0300

    Initial SeannaGarden backend + frontend setup

---
+++ server.js
@@ -0,0 +1,1 @@
+const express = require('express'); const app = express(); const port = 3000; app.get('/', (req,res)=>res.send('Hello Seanna Garden\!')); app.listen(port, ()=>console.log('🚀 Server running at http://localhost:' + port));
